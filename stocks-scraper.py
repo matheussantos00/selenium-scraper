c779cda5ce252c4b0dfa0e7138e85f2941e3f0d8
matheussantos00 <matheusphilippe00@gmail.com>
2020-05-06 22:58:21 -0300

    Functional script to login using google account

---
+++ stocks-scraper.py
@@ -0,0 +1,44 @@
+import login
+import sys
+import time
+from selenium import webdriver
+from bs4 import BeautifulSoup
+
+#firefox_profile = webdriver.FirefoxProfile()
+#firefox_profile.set_preference("browser.privatebrowsing.autostart", True)
+#driver = webdriver.Firefox(firefox_profile=firefox_profile)
+
+class Scrape:
+
+    def __init__(self):
+        self.url = 'https://br.tradingview.com/#signin'
+
+    def scrape(self):
+        #SELENIUM
+        driver = webdriver.Firefox()
+        driver.get(self.url)
+        print(driver.window_handles)
+        time.sleep(5)
+        login_page = driver.find_element_by_css_selector('span.tv-signin-dialog__social--big:nth-child(1)')
+        login_page.click()
+        print(driver.window_handles)
+        print(driver.current_window_handle)
+        driver.switch_to.window(driver.window_handles[1])
+        print(driver.current_window_handle)
+        time.sleep(5)
+        id = driver.find_element_by_id('identifierId')
+        id.send_keys('EMAIL')
+        time.sleep(5)
+        prox = driver.find_element_by_id('identifierNext')
+        prox.click()
+        time.sleep(5)
+        password = driver.find_element_by_name('password')
+        password.send_keys('PASSWORD')
+        time.sleep(5)
+        prox = driver.find_element_by_id('passwordNext')
+        prox.click()
+
+
+if __name__ != 'main':
+    app = Scrape()
+    app.scrape()
