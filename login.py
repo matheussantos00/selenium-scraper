4056c7163eaef997830f5e8bdcabfcb856017b34
matheussantos00 <matheusphilippe00@gmail.com>
2020-05-09 12:41:03 -0300

    login file update

--- login.py
+++ login.py
@@ -1,17 +1,17 @@
 class Login:
     def __init__(self):
-        pass
-
-    def login_file(self):
         try:
             file = open('login.txt', 'x')
-            file.write(input('login: ') + '\n')
+            file.write(input('email: ') + '\n')
             file.write(input('senha: '))
             file.close()
         except:
             pass
         finally:
             file = open('login.txt', 'r')
-            self.usuario = {'login': file.readline().replace('\n', ''),
-                            'password': file.readline()}
+            self.usuario = {'email': file.readline().replace('\n', ''),
+                            'senha': file.readline()}
             file.close()
+
+    def login_file(self):
+        pass
